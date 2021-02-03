5321ad158e69280df00a7e3d1b5b54dea31babc5
Cornelius-Mutisya <corneliusmutisya11@gmail.com>
2021-02-03 18:01:51 +0300

    Close & pass data in modal

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { ModalController, NavController } from "@ionic/angular";
 import { CreateBookingComponent } from "src/app/bookings/create-booking/create-booking.component";
+import { Place } from "../../place.model";
+import { PlacesService } from "../../places.service";
 
 @Component({
   selector: "app-place-detail",
@@ -9,13 +11,26 @@ import { CreateBookingComponent } from "src/app/bookings/create-booking/create-b
   styleUrls: ["./place-detail.page.scss"],
 })
 export class PlaceDetailPage implements OnInit {
+  place: Place;
+
   constructor(
     private router: Router,
     private navCtrl: NavController,
-    private modalCtrl: ModalController
+    private modalCtrl: ModalController,
+    private placesService: PlacesService,
+    private route: ActivatedRoute
   ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.route.paramMap.subscribe((paramMap) => {
+      if (!paramMap.has("placeId")) {
+        this.navCtrl.navigateBack("/places/tabs/offers");
+        return;
+      }
+      this.place = this.placesService.getPlace(paramMap.get("placeId"));
+      console.log(this.place.id);
+    });
+  }
 
   onBookPlace() {
     // this.router.navigateByUrl("/places/tabs/discover");
@@ -23,9 +38,19 @@ export class PlaceDetailPage implements OnInit {
     // this.navCtrl.pop();
 
     this.modalCtrl
-      .create({ component: CreateBookingComponent })
+      .create({
+        component: CreateBookingComponent,
+        componentProps: { selectedPlace: this.place },
+      })
       .then((modalEl) => {
         modalEl.present();
+        return modalEl.onDidDismiss();
+      })
+      .then((resultData) => {
+        console.log(resultData.data, resultData.role);
+        if (resultData.role === "confirm") {
+          console.log("BOOKED!");
+        }
       });
   }
 }
