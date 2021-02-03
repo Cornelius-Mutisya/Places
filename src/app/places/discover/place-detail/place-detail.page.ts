3a1973ff76c05bbfff7189491a5ba97b60a57e8a
Cornelius-Mutisya <corneliusmutisya11@gmail.com>
2021-02-03 14:54:15 +0300

    Open Modal

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
-import { NavController } from "@ionic/angular";
+import { ModalController, NavController } from "@ionic/angular";
+import { CreateBookingComponent } from "src/app/bookings/create-booking/create-booking.component";
 
 @Component({
   selector: "app-place-detail",
@@ -8,13 +9,23 @@ import { NavController } from "@ionic/angular";
   styleUrls: ["./place-detail.page.scss"],
 })
 export class PlaceDetailPage implements OnInit {
-  constructor(private router: Router, private navCtrl: NavController) {}
+  constructor(
+    private router: Router,
+    private navCtrl: NavController,
+    private modalCtrl: ModalController
+  ) {}
 
   ngOnInit() {}
 
   onBookPlace() {
     // this.router.navigateByUrl("/places/tabs/discover");
-    this.navCtrl.navigateBack("/places/tabs/discover");
+    // this.navCtrl.navigateBack("/places/tabs/discover");
     // this.navCtrl.pop();
+
+    this.modalCtrl
+      .create({ component: CreateBookingComponent })
+      .then((modalEl) => {
+        modalEl.present();
+      });
   }
 }
