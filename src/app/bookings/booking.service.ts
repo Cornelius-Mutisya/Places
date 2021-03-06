6599105642ddcf26ad753e6489cbac44fcb268b9
Cornelius-Mutisya <corneliusmutisya11@gmail.com>
2021-03-06 18:33:19 +0300

    Delete bookings

--- src/app/bookings/booking.service.ts
+++ src/app/bookings/booking.service.ts
@@ -96,19 +96,26 @@ export class BookingService {
             }
           }
           return bookings;
-        }), tap(bookings => {
+        }),
+        tap((bookings) => {
           this._bookings.next(bookings);
         })
       );
   }
 
   cancelBooking(bookingId: string) {
-    return this.bookings.pipe(
-      take(1),
-      delay(1000),
-      tap((bookings) => {
-        this._bookings.next(bookings.filter((b) => b.id !== bookingId));
-      })
-    );
+    return this.http
+      .delete(
+        `https://places-5ba5e-default-rtdb.firebaseio.com/bookings/${bookingId}.json`
+      )
+      .pipe(
+        switchMap(() => {
+          return this.bookings;
+        }),
+        take(1),
+        tap((bookings) => {
+          this._bookings.next(bookings.filter((b) => b.id !== bookingId));
+        })
+      );
   }
 }
